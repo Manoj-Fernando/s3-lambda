3aa186a50af8e732e02280937c582a6998e21285
Manoj-Fernando <manojfernando299@gmail.com>
2024-01-03 22:09:43 +0530

    initial commit

---
+++ src/main/java/com/aws/test/s3lambda/S3lambdaApplication.java
@@ -0,0 +1,13 @@
+package com.aws.test.s3lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3lambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3lambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/test/s3lambda/S3lambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.test.s3lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3lambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
