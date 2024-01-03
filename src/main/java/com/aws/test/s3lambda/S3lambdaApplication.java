9e605f34ab8c894603cc51c05d8f0566e799e7b7
Manoj-Fernando <manojfernando299@gmail.com>
2024-01-03 22:19:31 +0530

    initial commit

--- src/main/java/com/aws/test/s3lambda/S3lambdaApplication.java
+++ src/main/java/com/aws/test/s3lambda/S3lambdaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class S3lambdaApplication {
 
+	/*TODO: delete -- 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*/
 	public static void main(String[] args) {
 		SpringApplication.run(S3lambdaApplication.class, args);
 	}
